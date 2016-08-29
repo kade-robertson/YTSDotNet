232aaa38a4dbd1ee6f1de0e579317219623c3d89
Kade <kade.robertson16@gmail.com>
2016-08-28 23:48:52 -0400

    whoopsie, part of last commit

---
+++ Client/YTSClient.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace YTSDotNet
+{
+    public class YTSClient
+    {
+        private string m_appkey;
+        private bool m_hasappkey = false;
+        private JsonSerializer serializer = new JsonSerializer();
+
+        public YTSClient(string appkey = "")
+        {
+            if (appkey != "")
+            {
+                m_appkey = appkey;
+                m_hasappkey = true;
+            }
+        }
+
+        public async Task<ListMovieResult> ListMovies(int limit = 20, int page = 1, string quality = "All", int minimum_rating = 0, string query_term = "0", string genre = "", string sort_by = "date_added", string order_by = "desc", bool with_rt_ratings = false)
+        {
+            var format = "https://yts.ag/api/v2/list_movies.json?limit={0}&page={1}&quality={2}&minimum_rating={3}&query_term={4}&genre={5}&sort_by={6}&order_by={7}&with_rt_ratings={8}";
+            using (var client = new HttpClient())
+            {
+                var data = await client.GetStringAsync(string.Format(format,
+                                                                     limit,
+                                                                     page,
+                                                                     quality,
+                                                                     minimum_rating,
+                                                                     query_term,
+                                                                     genre,
+                                                                     sort_by,
+                                                                     order_by,
+                                                                     with_rt_ratings ? "true" : "false"));
+                var info = serializer.Deserialize<ListMovieResult>(new JsonTextReader(new StringReader(data)));
+                return info;
+            }
+        }
+    }
+}
